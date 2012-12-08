1b3c49182dc2a7003695f6f83b5fb3a501bd520c
Christopher Shireman <chris@wearehathway.com>
2012-12-08 07:34:17 -0800

    Updating date parser for API generated date strings

---
+++ DonationApp/models/NSDate+CDParser.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+CDParser.h
+//  DonationApp
+//
+//  Created by Chris Shireman on 12/8/12.
+//  Copyright (c) 2012 Chris Shireman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (CDParser)
+
+@end
---
+++ DonationApp/models/NSDate+CDParser.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+CDParser.m
+//  DonationApp
+//
+//  Created by Chris Shireman on 12/8/12.
+//  Copyright (c) 2012 Chris Shireman. All rights reserved.
+//
+
+#import "NSDate+CDParser.h"
+
+@implementation NSDate (CDParser)
+
+@end
